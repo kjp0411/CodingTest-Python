9bc80199e54eebf1f45517e308f90aa60c7245a8
kjp0411 <98109773+kjp0411@users.noreply.github.com>
2025-10-07 20:40:12 +0900

    알고리즘 코딩 테스트 day25 문제 091 ~ 093번

---
+++ 알고리즘 코딩 테스트/day25/091_계단 수 구하기.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/10844
+N = int(input())
+D = [[0 for j in range(11)] for i in range(N + 1)]
+MOD = 1000000000
+
+for i in range(1, 10):
+    D[1][i] = 1
+
+for i in range(2, N + 1):
+    D[i][0] = D[i - 1][1]
+    D[i][9] = D[i - 1][8]
+    for j in range(1, 9):
+        D[i][j] = (D[i - 1][j - 1] + D[i - 1][j + 1])
+
+sum_value = 0
+
+for i in range(10):
+    sum_value = (sum_value + D[N][i]) % MOD
+
+print(sum_value)
---
+++ 알고리즘 코딩 테스트/day25/092_연속된 정수의 합 구하기.py
@@ -0,0 +1,26 @@
+# https://www.acmicpc.net/problem/13398
+N = int(input())
+A = list(map(int, input().split()))
+
+# 오른쪽으로 index를 포함한 최대 연속 합 구하기
+L = [0] * N
+L[0] = A[0]
+result = L[0]
+
+for i in range(1, N):
+    L[i] = max(A[i], L[i - 1] + A[i])
+    result = max(result, L[i])
+
+# 왼쪽으로 index를 포함한 최대 연속 합 구하기
+R = [0] * N
+R[N - 1] = A[N - 1]
+
+for i in range(N - 2, -1, -1):
+    R[i] = max(A[i], R[i + 1] + A[i])
+
+# L[i - 1] + R[i + 1] 2개의 구간 합 리스트를 더하면 i번째 값을 제거한 효과를 얻음
+for i in range(1, N - 1):
+    temp = L[i - 1] + R[i + 1]
+    result = max(result, temp)
+
+print(result)
