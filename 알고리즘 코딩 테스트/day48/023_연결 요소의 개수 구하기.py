2d31fe590c6679725ec5a4673fe883eefe4c2260
kjp0411 <98109773+kjp0411@users.noreply.github.com>
2025-10-30 17:23:20 +0900

    알고리즘 코딩 테스트 day48 문제 15, 23, 26번(4회독)

---
+++ 알고리즘 코딩 테스트/day48/023_연결 요소의 개수 구하기.py
@@ -0,0 +1,27 @@
+# https://www.acmicpc.net/problem/11724
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+N, M = map(int, input().split())
+A = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
+
+def DFS(v):
+    visited[v] = True
+    for i in A[v]:
+        if not visited[i]:
+            DFS(i)
+
+for _ in range(M):
+    s, e = map(int, input().split())
+    A[s].append(e)
+    A[e].append(s)
+
+count = 0
+
+for i in range(1, N + 1):
+    if not visited[i]:
+        count += 1
+        DFS(i)
+
+print(count)
